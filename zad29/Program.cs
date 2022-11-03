e36fb5115b24c6cc3298ac8dc03839ad75cb61dc
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-03 11:18:02 +0300

    решили задачу

--- zad29/Program.cs
+++ zad29/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая задаёт массив из N элементов и выводит их на экран
+Console.Clear();
+Console.WriteLine("Ввелите количество элементов массива  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int[] array = new int[N];
+// Рандомный ввод массива
+    Random rand = new Random();
+    for (int i = 0; i < array.Length; i++)
+     {
+      array[i] = rand.Next(1,9);
+     }
+//Вывод массива 
+            Console.Write("Исходный массив - ");
+            for (int i = 0; i < array.Length; i++)
+            Console.Write(" " + array[i]);
+            Console.WriteLine();
