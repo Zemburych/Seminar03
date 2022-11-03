0a94b576b5ce7ba8abed3f2d064083d508929c72
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-03 11:34:52 +0300

    решили задачу 27

---
+++ zad27/Program.cs
@@ -0,0 +1,12 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Clear();
+Console.Write("Ввелите число -  ");
+int N = int.Parse(Console.ReadLine());
+int summ = 0;
+while (N > 0)
+            {
+                 summ = summ + N % 10;
+                N = N / 10 ;
+             }
+ Console.WriteLine(summ);
