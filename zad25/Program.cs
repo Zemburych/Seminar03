ae89b3a51c55170bc65ea03f867e75084b71261f
Zemburych <116352585+Zemburych@users.noreply.github.com>
2022-11-03 20:24:15 +0300

    сохранили текущий процесс. Задача решена

--- zad25/Program.cs
+++ zad25/Program.cs
@@ -1,20 +1,31 @@
 ﻿// Задача 25: Напишите метод, который принимает на вход два числа (A и B) и возводит число A в целую степень B.
 Console.Clear();
 Console.Write("Ввелите число А - ");
-int A = int.Parse(Console.ReadLine());
+int a = int.Parse(Console.ReadLine());
 Console.Write("Ввелите число B - ");
-int B = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
 
-int res = A;
-int i = 1;
-void degree(int A, int B)
-{
-    while (i<B)
-    {
-      res = res * A; 
-      i++;
-    }
+// int res = A;
+// int i = 1;
+// void degree(int A, int B)
+// {
+//     while (i<B)
+//     {
+//       res = res * A; 
+//       i++;
+//     }
+// }
+// return res;
+// degree(A, B);
+// Console.WriteLine(res);
+
+double Pow(int a, int b)
+{ 
+  bool flag = b > 0;
+  if (b < 0) b = -b;
+  int res = 1;
+  for (int i = 0; i < b; i++) res *=a;
+  if (flag) return res;
+  else return 1.0 / res;
 }
-return res;
-degree(A, B);
-Console.WriteLine(res);
+Console.WriteLine("А в степени B = " + Pow(a, b));
